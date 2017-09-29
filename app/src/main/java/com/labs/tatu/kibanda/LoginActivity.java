b48d39112ee20aaf9cec09925d92bfb68951862a
David Amunga <david.onyimbo@gmail.com>
2017-09-29 14:00:21 +0300

    Initial commit

---
+++ app/src/main/java/com/labs/tatu/kibanda/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.labs.tatu.kibanda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edtPhone,edtPassword;
+    Button signIn;
+
+    DatabaseReference mDatabase;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initViews();
+
+
+//        Initialize Firebase
+        mDatabase= FirebaseDatabase.getInstance().getReference("User");
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Get User Information
+                        
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void initViews() {
+        edtPassword=(EditText)findViewById(R.id.edtPassword);
+        edtPhone=(EditText)findViewById(R.id.edtPhone);
+        signIn=(Button)findViewById(R.id.btnSignIn);
+    }
+}
---
+++ app/src/main/java/com/labs/tatu/kibanda/MainActivity.java
@@ -0,0 +1,48 @@
+package com.labs.tatu.kibanda;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn,btnSignUp;
+    TextView txtSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        Typeface face=Typeface.createFromAsset(getAssets(),"nabila.ttf");
+        txtSlogan.setTypeface(face);
+
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private void initViews() {
+        btnSignIn=(Button)findViewById(R.id.btnSignIn);
+        btnSignUp=(Button)findViewById(R.id.btnSignUp);
+        txtSlogan=(TextView)findViewById(R.id.txtSlogan);
+
+
+    }
+}
