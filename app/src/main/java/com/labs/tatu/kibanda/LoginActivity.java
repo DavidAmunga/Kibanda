d3b893373c7daacd0febd6af1b5ef3f235c5e6b3
David Amunga <david.onyimbo@gmail.com>
2017-10-07 09:08:07 +0300

    Show Details of Food

--- app/src/main/java/com/labs/tatu/kibanda/LoginActivity.java
+++ app/src/main/java/com/labs/tatu/kibanda/LoginActivity.java
@@ -24,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     Button signIn;
 
     DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/labs/tatu/kibanda/MainActivity.java
+++ app/src/main/java/com/labs/tatu/kibanda/MainActivity.java
@@ -2,16 +2,24 @@ package com.labs.tatu.kibanda;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     Button btnSignIn,btnSignUp;
     TextView txtSlogan;
+
+    FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
 
     private void initViews() {
         btnSignIn=(Button)findViewById(R.id.btnSignIn);
