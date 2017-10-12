3d85d3acfddf1c7377b2c07d17209c2c412bee62
David Amunga <david.onyimbo@gmail.com>
2017-10-12 10:38:39 +0300

    Create New Category

--- app/src/main/java/com/labs/tatu/kibanda/LoginActivity.java
+++ app/src/main/java/com/labs/tatu/kibanda/LoginActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,6 +13,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.labs.tatu.kibanda.admin.HomeAdmin;
 import com.labs.tatu.kibanda.common.Common;
 import com.labs.tatu.kibanda.model.User;
 import com.rengwuxian.materialedittext.MaterialEditText;
@@ -39,7 +39,8 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 final ProgressDialog mDialog = new ProgressDialog(LoginActivity.this);
-                mDialog.setMessage("Please waiting....");
+                mDialog.setMessage("Please wait....");
+                mDialog.setCancelable(false);
                 mDialog.show();
 
                 mDatabase.addValueEventListener(new ValueEventListener() {
@@ -54,11 +55,20 @@ public class LoginActivity extends AppCompatActivity {
                             User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
                             user.setPhone(edtPhone.getText().toString());
                             if (user.getPassword().equals(edtPassword.getText().toString())) {
-                                Intent homeIntent = new Intent(LoginActivity.this, Home.class);
+                                if (user.getName().equals("admin")) {
+                                    Intent homeIntent = new Intent(LoginActivity.this, HomeAdmin.class);
 //                                Create Variable to save current user
-                                Common.currentUser = user;
-                                startActivity(homeIntent);
-                                finish();
+                                    Common.currentUser = user;
+                                    startActivity(homeIntent);
+                                    finish();
+                                } else {
+                                    Intent homeIntent = new Intent(LoginActivity.this, Home.class);
+//                                Create Variable to save current user
+                                    Common.currentUser = user;
+                                    startActivity(homeIntent);
+                                    finish();
+                                }
+
 
                             } else {
                                 Toast.makeText(LoginActivity.this, "Wrong Password!", Toast.LENGTH_SHORT).show();
